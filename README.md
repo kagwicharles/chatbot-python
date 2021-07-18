35fbbde58daf49b3d95c262e1a0312a7b942b380
kagwicharles <ckagwi8@gmail.com>
2021-07-18 15:29:04 +0300

    new version

--- README.md
+++
@@ -1,2 +0,0 @@
-# chatbot-python
-Simple chatbot implement using python socket-client arcthitecture
--- server.py
+++ server.py
@@ -1,7 +1,6 @@
-import socket
+from flask import Flask, json, request
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
 
 chatbot = ChatBot('Ron')
 conversation = [
@@ -23,33 +22,14 @@ trainer = ChatterBotCorpusTrainer(chatbot)
 # Train the chatbot based on the english corpus
 trainer.train("chatterbot.corpus.english")
 
-def server_program():
-    # get the hostname
-    host = socket.gethostname()
-    port = 5000  # initiate port no above 1024
+api = Flask(__name__)
 
-    server_socket = socket.socket()  # get instance
-    # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
-
-    # configure how many client the server can listen simultaneously
-    server_socket.listen(2)
-    print("Server started...")
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
-    while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        bot_input = chatbot.get_response(data)
-        conn.send(str(bot_input).encode())  # send data to the client
-
-    conn.close()  # close the connection
-
+@api.route('/chatbot', methods=['POST'])
+def get_companies():
+	message = request.form.get('message')
+	bot_input = chatbot.get_response(message)
+	companies = {"message": str(bot_input)}
+	return json.dumps(companies)
 
 if __name__ == '__main__':
-    server_program()
-
+    api.run(host='0.0.0.0', port=5000)
